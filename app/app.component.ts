fc2697275c4a83d545673d55ba8d820506745ef6
Stanimir Todorov <stanimirtt@gmail.com>
2016-02-12 15:01:40 +0200

    Add material.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component }       from 'angular2/core';
 import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from 'angular2/router';
 
+import {MATERIAL_DIRECTIVES} from "ng2-material/all";
+
 import { HeroService }     from './hero.service';
 import { HeroesComponent } from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component';
@@ -9,18 +11,12 @@ import { SelectComponent } from './select.component';
 
 @Component({
 	selector: 'my-app',
-	template: `
-	  <h1>{{title}}</h1>
-	  <nav>
-	    <a [routerLink]="['Dashboard']">Dashboard</a>
-	    <a [routerLink]="['Heroes']">Heroes</a>
-	  </nav>
-	  <router-outlet></router-outlet>
-	`,
+	templateUrl: 'app/app.component.html',
 	styleUrls: ['app/app.component.css'],
 	directives: [ROUTER_DIRECTIVES],
 	providers: [
 		ROUTER_PROVIDERS,
+		MATERIAL_DIRECTIVES,
 		HeroService
 	]
 })
@@ -42,11 +38,11 @@ import { SelectComponent } from './select.component';
 		name: 'HeroDetail',
 		component: HeroDetailComponent
 	},
-	// {
-	// 	path: '/select',
-	// 	name: 'Select',
-	// 	component: SelectComponent
-	// }
+	{
+		path: '/select',
+		name: 'Select',
+		component: SelectComponent
+	}
 ])
 
 export class AppComponent {
