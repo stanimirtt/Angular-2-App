3ebe72db82d00758bd7ea591147b05cfa0bc91ec
Stanimir Todorov <stanimirtt@gmail.com>
2016-02-23 18:10:33 +0200

    Refactor the services

--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,11 @@
-import {bootstrap}    from 'angular2/platform/browser'
 import {HTTP_PROVIDERS } from 'angular2/http';
+import {Component, Injectable} from 'angular2/core';
+import {LittleKittyService} from 'ng2-little-kitty/ng2-little-kitty';
+
+import {bootstrap}    from 'angular2/platform/browser'
 import {AppComponent} from './app.component'
 
-bootstrap(AppComponent, [HTTP_PROVIDERS]);
+bootstrap(AppComponent, [
+	HTTP_PROVIDERS,
+	LittleKittyService
+]);
